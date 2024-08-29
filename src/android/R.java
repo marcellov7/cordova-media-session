f50a0a9b01732c27668184c7fee48ad03f443c0a
marcellov7 <marcelloviolini92@gmail.com>
2024-08-30 01:38:41 +0200

    up

---
+++ src/android/R.java
@@ -0,0 +1,14 @@
+package com.marcellov7.cordova.mediasession;
+
+public final class R {
+    public static final class drawable {
+        public static final int ic_baseline_volume_up_24 = 0x7f020000;
+        public static final int ic_baseline_play_arrow_24 = 0x7f020001;
+        public static final int ic_baseline_pause_24 = 0x7f020002;
+        public static final int ic_baseline_replay_30_24 = 0x7f020003;
+        public static final int ic_baseline_forward_30_24 = 0x7f020004;
+        public static final int ic_baseline_skip_previous_24 = 0x7f020005;
+        public static final int ic_baseline_skip_next_24 = 0x7f020006;
+        public static final int ic_baseline_stop_24 = 0x7f020007;
+    }
+}
